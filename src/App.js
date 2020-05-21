25df840a197f93d4b40586a3cc51427adfa95c62
Rawand Dargali <rdargali@yahoo.com>
2020-05-20 21:57:50 -0500

    cleanup

--- src/App.js
+++ src/App.js
@@ -2,13 +2,8 @@ import React from "react";
 
 import "./App.css";
 
-function App() {
-  console.log(process.env.REACT_APP_API_KEY);
-  return (
-    <div className="App">
-      <h1>weather!</h1>
-    </div>
-  );
-}
+const App = () => {
+  return <div className="App"></div>;
+};
 
 export default App;
